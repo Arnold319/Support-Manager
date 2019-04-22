6e842604e96d63b9bc3400a6985d9062d970bba1
Arnold319 <49011323+Arnold319@users.noreply.github.com>
2019-04-22 21:12:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -766,4 +766,4 @@ async def uptime_count():
 
 
 client.loop.create_task(uptime_count())
-client.run('BOT-TOKEN')  # TODO: insert token
+client.run('NTU5OTA5MjU2MDk5MjY2NTYw.XL3gfQ.sG2DxAluSyTaZZAwjk2n9nXtFE8')  # TODO: insert token
